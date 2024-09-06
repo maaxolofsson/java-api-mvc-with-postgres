e0310c1721a87ac90bb85e23276c9c384b49d090
Max V Olofsson <max.olofsson@se.experis.com>
2024-09-06 16:20:19 +0200

    Refractor in EmployeeReposotiry for DB connection

--- src/main/java/com/booleanuk/api/repository/EmployeeRepository.java
+++ src/main/java/com/booleanuk/api/repository/EmployeeRepository.java
@@ -1,58 +1,20 @@
 package com.booleanuk.api.repository;
 
-import javax.sql.DataSource;
-
+import com.booleanuk.api.db.DatabaseConnection;
 import com.booleanuk.api.model.Employee;
-import org.postgresql.ds.PGSimpleDataSource;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
 
 public class EmployeeRepository {
 
-    private DataSource datasource;
-    private String dbUser;
-    private String dbURL;
-    private String dbPassword;
-    private String dbDatabase;
-    private Connection connection;
-
     public EmployeeRepository() throws SQLException {
-        this.getDatabaseConnection();
-        this.datasource = this.createDataSource();
-        this.connection = this.datasource.getConnection();
-    }
-
-    private DataSource createDataSource() {
-        final String url = "jdbc:postgresql://" + this.dbURL + ":5432/" + this.dbDatabase + "?user=" + this.dbUser + "&password=" + this.dbPassword;
-        final PGSimpleDataSource dataSource = new PGSimpleDataSource();
-        dataSource.setUrl(url);
-        return dataSource;
-    }
-
-    private void getDatabaseConnection() {
-        InputStream input;
-        try {
-            input = new FileInputStream("src/main/resources/config.properties");
-
-            Properties prop = new Properties();
-            prop.load(input);
-            this.dbUser = prop.getProperty("db.user");
-            this.dbURL = prop.getProperty("db.url");
-            this.dbPassword = prop.getProperty("db.password");
-            this.dbDatabase = prop.getProperty("db.database");
-        } catch (Exception e) {
-            System.out.println("Error: " + e);
-        }
     }
 
     public List<Employee> getall() throws SQLException {
         List<Employee> toReturn = new ArrayList<>();
-        PreparedStatement statement = this.connection.prepareStatement("SELECT * FROM employees");
+        PreparedStatement statement = DatabaseConnection.getConnection().prepareStatement("SELECT * FROM employees");
 
         ResultSet res = statement.executeQuery();
 
@@ -72,7 +34,7 @@ public class EmployeeRepository {
 
 
     public Employee getOne(long id) throws SQLException {
-        PreparedStatement statement = this.connection.prepareStatement("SELECT * FROM employees WHERE id = ?");
+        PreparedStatement statement = DatabaseConnection.getConnection().prepareStatement("SELECT * FROM employees WHERE id = ?");
         statement.setLong(1, id);
 
         ResultSet res = statement.executeQuery();
@@ -91,7 +53,7 @@ public class EmployeeRepository {
 
     public Employee add(Employee employee) throws SQLException {
         String query = "INSERT INTO employees (name, jobName, salaryGrade, department) VALUES (?, ?, ?, ?)";
-        PreparedStatement statement = this.connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+        PreparedStatement statement = DatabaseConnection.getConnection().prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
         statement.setString(1, employee.getName());
         statement.setString(2, employee.getJobName());
         statement.setString(3, employee.getSalaryGrade());
@@ -117,7 +79,7 @@ public class EmployeeRepository {
 
     public Employee delete(long id) throws SQLException {
         String query = "DELETE FROM employees WHERE id = ?";
-        PreparedStatement statement = this.connection.prepareStatement(query);
+        PreparedStatement statement = DatabaseConnection.getConnection().prepareStatement(query);
         Employee toDelete = null;
         toDelete = this.getOne(id);
 
@@ -136,7 +98,7 @@ public class EmployeeRepository {
                 "salaryGrade = ? ," +
                 "department = ? " +
                 "WHERE id = ? ";
-        PreparedStatement statement = this.connection.prepareStatement(SQL);
+        PreparedStatement statement = DatabaseConnection.getConnection().prepareStatement(SQL);
         statement.setString(1, newData.getName());
         statement.setString(2, newData.getJobName());
         statement.setString(3, newData.getSalaryGrade());
