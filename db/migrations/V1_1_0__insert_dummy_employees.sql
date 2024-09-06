9d66cdb29de70f22f25078d67b349ef12b728726
Max V Olofsson <max.olofsson@se.experis.com>
2024-09-06 13:21:33 +0200

    Adding

---
+++ db/migrations/V1_1_0__insert_dummy_employees.sql
@@ -0,0 +1,12 @@
+-- Salary grades
+-- CEO, Senior Director , Director, Senior Manager, Manager, Senior Employee, Employee
+
+INSERT INTO employees
+    (name, jobName, salaryGrade, department)
+VALUES
+    ('Max Olofsson', 'CEO', 'CEO', 'Management'),
+    ('Test Testsson', 'Software developer', 'Employee', 'IT'),
+    ('Chad Svensson', 'Receptionist', 'Employee', 'Service'),
+    ('Alan Turing', 'Mathematician', 'Senior Employee', 'Education'),
+    ('Sven Svensson', 'CTO', 'Senior Director', 'Management'),
+    ('Jack Smith', 'Team leader', 'Manager', 'IT');
