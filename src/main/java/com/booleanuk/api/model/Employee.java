e34d715ecc929f15af28f0faacb9dcd5a6897112
Max V Olofsson <max.olofsson@se.experis.com>
2024-09-06 13:01:33 +0200

    Created controller, repository, and model files. Added code to Employee model

---
+++ src/main/java/com/booleanuk/api/model/Employee.java
@@ -0,0 +1,47 @@
+package com.booleanuk.api.model;
+
+public class Employee {
+
+    private long id;
+    private String name;
+    private String jobName;
+    private String salaryGrade;
+    private String department;
+
+    public Employee(long id, String name, String jobName, String salaryGrade, String department) {
+        this.id = id;
+        this.name = name;
+        this.jobName = jobName;
+        this.salaryGrade = salaryGrade;
+        this.department = department;
+    }
+
+    public Employee() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getJobName() {
+        return jobName;
+    }
+
+    public String getSalaryGrade() {
+        return salaryGrade;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setId(long newId) {
+        this.id = newId;
+    }
+
+}
---
+++ src/main/java/com/booleanuk/api/repository/EmployeeRepository.java
@@ -0,0 +1,4 @@
+package com.booleanuk.api.repository;
+
+public class EmployeeRepository {
+}
