38ba32c530755a45444c960f4ca4f85be55038ef
Max V Olofsson <max.olofsson@se.experis.com>
2024-09-06 14:04:48 +0200

    Added route for /employees, getAll()

--- src/main/java/com/booleanuk/api/controller/EmployeeController.java
+++ src/main/java/com/booleanuk/api/controller/EmployeeController.java
@@ -1,4 +1,27 @@
 package com.booleanuk.api.controller;
 
+import com.booleanuk.api.model.Employee;
+import com.booleanuk.api.repository.EmployeeRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@RestController
+@RequestMapping("employees")
 public class EmployeeController {
+
+    private EmployeeRepository employees;
+
+    public EmployeeController() throws SQLException {
+        this.employees = new EmployeeRepository();
+    }
+
+    @GetMapping
+    public List<Employee> getAll() throws SQLException {
+        return this.employees.getall();
+    }
+
 }
